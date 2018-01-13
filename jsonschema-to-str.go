4125d21d610fd20b56e9e4036bd86ff3d10193eb
voldan <vladdan1980@gmail.com>
2018-01-13 22:33:06 +0200

    added validation of JSON file

--- jsonschema-to-str.go
+++ jsonschema-to-str.go
@@ -7,25 +7,40 @@ import (
     "io/ioutil"
     "fmt"
     "strings"
+    "encoding/json"
 )
 
-func isJsonFile(fileName string) bool {
+func isJSONFile(fileName string) bool {
     if filepath.Ext(fileName) == ".json" {
         return true
     }
     return false
 }
 
+func validateJSONObject(content []byte) error {
+    var j map[string]interface{}
+    err := json.Unmarshal(content, &j)
+    if err != nil {
+        return err
+    }
+    return nil
+}
+
 func main()  {
     fileName := os.Args[1]
-    if !isJsonFile(fileName) {
-        log.Fatal("Error: input file extension is not a json")
+    if !isJSONFile(fileName) {
+        log.Fatal("Error: input file extension is not json")
     }
 
     content, err := ioutil.ReadFile(fileName)
     if err != nil {
         log.Fatal(err)
     }
+
+    if err = validateJSONObject(content); err != nil {
+        log.Fatalf("Invalid json schema file '%s', error: %v", fileName, err)
+    }
+
     contentStr := string(content)
     contentStr = strings.Replace(contentStr, "\n", "", -1)
     contentStr = strings.Replace(contentStr, " ", "", -1)
